88e0cc6375a588e557c98f1b9c4d84c9ea6db448
Luke Drogan <luke@super-wide.com>
2021-03-12 19:09:40 +0000

    countdown timer added to code

--- Assets/script.js
+++ Assets/script.js
@@ -4,15 +4,9 @@ Create an Event Listener for the button, which starts a countdown timer
 */
 
 
-var timeLeft = 120;
-var startButton = document.getElementById('startButton');
-var timeEl = document.getElementById('timer');
-
-startButton.addEventListener('click', setTime())
-
 //Set up a variable for Quiz questions array
 
-var quizQuestions = [
+var quizArray = [
     {
         title: "Question 1",
         choices: ["a", "b", "c", "d"],
@@ -40,3 +34,39 @@ var quizQuestions = [
     },
 
 ];
+
+//use query selectors to return HTML elements
+
+var countdown = document.querySelector("#countdown");
+var startQuizBtn = document.querySelector("#startButton");
+var questions = document.querySelector("#questions");
+
+
+//test querySelectors:
+
+console.log(countdown);
+console.log(startButton);
+console.log(questions);
+
+/*Set up timer display to start a countdown when start quiz button is pressed.
+Adapted from 09-Ins_Timers-Interval:*/
+
+
+startQuizBtn.addEventListener("click", function() {
+    var timeLeft = 81;
+    var timeInterval = 0;
+
+    if (timeInterval === 0) {
+        timeInterval = setInterval(function () {
+            timeLeft--;
+            countdown.textContent = timeLeft;
+
+            if (timeLeft <= 0) {
+                clearInterval(timeInterval);
+                allDone();
+                countdown.textContent = "Game Over";
+            }
+        }, 1000);
+    }
+  });
+
