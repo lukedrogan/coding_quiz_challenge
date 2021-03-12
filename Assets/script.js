3361c6e92c088ecdabb908fe2f61c354ff62ebdb
Luke Drogan <luke@super-wide.com>
2021-03-12 12:06:25 +0000

    added questions array & floated Highscores & Timer

--- Assets/script.js
+++ Assets/script.js
@@ -9,3 +9,34 @@ var startButton = document.getElementById('startButton');
 var timeEl = document.getElementById('timer');
 
 startButton.addEventListener('click', setTime())
+
+//Set up a variable for Quiz questions array
+
+var quizQuestions = [
+    {
+        title: "Question 1",
+        choices: ["a", "b", "c", "d"],
+        answer: "answer"
+    },
+    {
+        title: "Question 2",
+        choices: ["a", "b", "c", "d"],
+        answer: "answer"
+    },
+    {
+        title: "Question 3",
+        choices: ["a", "b", "c", "d"],
+        answer: "answer"
+    },
+    {
+        title: "Question 4",
+        choices: ["a", "b", "c", "d"],
+        answer: "answer"
+    },
+    {
+        title: "Question 5",
+        choices: ["a", "b", "c", "d"],
+        answer: "answer"
+    },
+
+];
