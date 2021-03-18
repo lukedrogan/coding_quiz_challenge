7d9347effee22aa986513d9f798eba4acb380a43
Luke Drogan <luke@super-wide.com>
2021-03-18 18:36:59 +0000

    issues with highscore page rectified & code improved

--- Assets/highscore.js
+++ Assets/highscore.js
@@ -2,26 +2,27 @@ var highScore = document.querySelector("#highScore");
 var reset = document.querySelector("#reset");
 var goBack = document.querySelector("#goBack");
 
-// Clear scores using event listener 
-clear.addEventListener("click", function () {
-    localStorage.clear();
-    location.reload();
-});
-// Retreives local stroage 
-var allScores = localStorage.getItem("allScores");
-allScores = JSON.parse(allScores);
+function showHighscores() {
+  // Retreives local stroage
+  var allScores = localStorage.getItem("highscores");
+  allScores = JSON.parse(allScores);
 
-if (allScores !== null) {
-
+  if (allScores !== null) {
     for (var i = 0; i < allScores.length; i++) {
-
-        var createLi = document.createElement("li");
-        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
-        highScore.appendChild(createLi);
-
+      var createLi = document.createElement("li");
+      createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+      highScore.appendChild(createLi);
     }
+  }
 }
 // Event listener to move to index page
 goBack.addEventListener("click", function () {
-    window.location.replace("./index.html");
+  window.location.replace("./index.html");
 });
+
+// Clear scores using event listener
+reset.addEventListener("click", function () {
+  localStorage.clear();
+  location.reload();
+});
+showHighscores();
--- Assets/script.js
+++ Assets/script.js
@@ -54,6 +54,7 @@ var score = 0;
 var ulCreate = document.createElement("ul");
 var timeLeft = 81;
 var timeInterval = 0;
+var createDiv;
 
 startQuizBtn.addEventListener("click", function() {
     
@@ -70,32 +71,30 @@ startQuizBtn.addEventListener("click", function() {
             }
         }, 1000);
     }
-    render(questionsIndex);
+    render();
   });
 
 // Adds the questions and multiple choice options to the page: 
-function render(questionsIndex) {
+function render() {
     // clear existing data on page
     questions.innerHTML = "";
     ulCreate.innerHTML = "";
     
     // Loop through all info in array
-    for (var i = 0; i < quizArray.length; i++) {
+    var askQuestion = quizArray[questionsIndex].title;
+    var showAnswers = quizArray[questionsIndex].choices;
+    var questionTitle = document.createElement("h3")
+    questionTitle.textContent = askQuestion;
+    questions.appendChild(questionTitle)
+    for (var i = 0; i < showAnswers.length; i++) {
 
-        // Add question "titles" to page 
-        var askQuestion = quizArray[questionsIndex].title;
-        var showAnswers = quizArray[questionsIndex].choices;
-        questions.textContent = askQuestion;
-    }
-    
-    // New for each for question choices
-    showAnswers.forEach(function (newItem) {
         var listItem = document.createElement("li");
-        listItem.textContent = newItem;
+        listItem.textContent = showAnswers[i];
+        listItem.addEventListener("click", (compare));
         questions.appendChild(ulCreate);
         ulCreate.appendChild(listItem);
-        listItem.addEventListener("click", (compare));
-    })
+        
+    }
 }
 
 function compare(event) {
@@ -110,31 +109,34 @@ function compare(event) {
         if (element.textContent === quizArray[questionsIndex].answer) {
             score++;
             createDiv.textContent = "You got the answer right!!";
+            alert("Correct")
            
         } else {
             // Deduct 10 seconds from timeLeft for every wrong answer
             var penalty = 10;
             timeLeft = timeLeft - penalty;
             createDiv.textContent = "Wrong! The correct answer is:  " + quizArray[questionsIndex].answer;
+            alert("Wrong")
+        }
+        // Question Index determines number question user is on
+        questionsIndex++;
+        if (questionsIndex === quizArray.length) {
+            // All done will append last page with user stats
+            allDone();
+        } else {
+            render();
         }
     }
-
-// Question Index determines number question user is on
-questionsIndex++;
-
-if (questionsIndex >= quizArray.length) {
-    // All done will append last page with user stats
-    allDone();
-    createDiv.textContent = "End of quiz!" + " " + "You got  " + score + "question(s) right!!"
-} else {
-    render(questionsIndex);
-}
-questions.appendChild(createDiv);
-
+    
+    
+    
 }
 // All done will append last page
 function allDone() {
     questions.innerHTML = "";
+    var finalMessage = document.createElement("h3")
+    finalMessage.textContent = "End of quiz!" + " " + "You got  " + score + " question(s) right!!"
+    questions.appendChild(finalMessage);
     countdown.innerHTML = "";
 
     // Heading:
@@ -187,9 +189,9 @@ questions.appendChild(createSubmit);
 createSubmit.addEventListener("click", function () {
     var initials = createInput.value;
 
-    if (initials === null) {
+    if (initials.length === 0) {
 
-        console.log("Please enter your initials");
+        alert("Please enter your initials")
 
     } else {
         var finalScore = {
@@ -197,17 +199,12 @@ createSubmit.addEventListener("click", function () {
             score: timeRemaining
         }
         console.log(finalScore);
-        var allScores = localStorage.getItem("allScores");
-        if (allScores === null) {
-            allScores = [];
-        } else {
-            allScores = JSON.parse(allScores);
-        }
-        allScores.push(finalScore);
-        var newScore = JSON.stringify(allScores);
-        localStorage.setItem("allScores", newScore);
+       var gameScores = JSON.parse(window.localStorage.getItem("highscores")) || []; 
+           gameScores.push(finalScore);
+           window.localStorage.setItem("highscores", JSON.stringify(gameScores))
+    
         
-        window.location.replace("./HighScores.html");
+        window.location.replace("./highscore.html");
     }
 });
 
